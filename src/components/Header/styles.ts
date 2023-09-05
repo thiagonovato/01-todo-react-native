58d5e984a57e05670194682098339605375367ba
Thiago Novato <thiagonovato@gmail.com>
2023-09-05 07:16:45 -0400

    Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../utils/colors';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.gray700,
+    padding: 24,
+    height: 173,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
